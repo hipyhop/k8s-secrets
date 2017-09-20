cbeffdb8f20dfd91ad41d64951808c2740629290
Thomas Luff <tomfluff@gmail.com>
2017-09-20 21:56:15 +1000

    Add first version of tool

---
+++ main.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+"use strict";
+const YAML = require("yamljs");
+
+var method = process.argv[2]
+var fname = process.argv[3];
+
+var func;
+if (!method || (method !== "decode" && method !== "encode")) {
+  consol.error("Unknown command '" + method + "'. Must be one of 'encode' & 'decode'.");
+  process.exit(1);
+}
+if (method === "decode") {
+  func = v => (Buffer.from(v, "base64")).toString();
+} else {
+  func = v => new Buffer(v).toString("base64");
+}
+
+if (!fname) {
+  console.error("2nd argument must be a yaml file");
+  process.exit(1);
+}
+
+
+var obj = YAML.load(fname);
+
+for (let key in obj.data) {
+  obj.data[key] = func(obj.data[key]);
+}
+
+
+console.log(YAML.stringify(obj, 2));
